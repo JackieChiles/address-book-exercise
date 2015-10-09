395b5fc63807fa0e9181550161eb060be42c80c7
JackieChiles <dtgaynor@gmail.com>
2015-10-09 16:15:26 -0400

    Implemented initial, basic version.

--- client/src/controllers/indexCtrl.js
+++ client/src/controllers/indexCtrl.js
@@ -1,6 +1,19 @@
 angular.module('addressBook').controller('indexCtrl', [
   '$scope',
-  function($scope) {
-    $scope.message = 'Happy coding!';
+  '$log',
+  'apiService',
+  function($scope, $log, apiService) {
+    //Load all people data from the API
+    apiService.getPeople()
+        .then(function (response) {
+            $scope.people = response.data.people;
+        }, function (response) {
+            $log.error('Error when retrieving people data: ', response);
+        });
+
+    //Functions
+    $scope.select = function (person) {
+        $scope.selectedPerson = person;
+    }
   }
 ]);
